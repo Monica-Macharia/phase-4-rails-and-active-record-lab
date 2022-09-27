736060f874022241a15de2da5c55015cb95d5f1a
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-09-27 20:51:20 +0300

    created a model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+Student.create!(first_name: 'Cheddar', last_name: 'Fofo', grade: 3)
+Student.create!(first_name: 'Chedda', last_name: 'Foo', grade: 23)
+Student.create!(first_name: 'Cdar', last_name: 'ofo', grade: 53)
